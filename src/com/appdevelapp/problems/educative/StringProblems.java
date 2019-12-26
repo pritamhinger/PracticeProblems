4acaa44b47bb3c487254859d0214b93177872692
Pritam Hinger <pritamgenius24@gmail.com>
2019-12-26 12:15:34 +0530

    Adding solution to String Segmentaoin. XML to Tree and Palindrome SubStrings.

--- src/com/appdevelapp/problems/educative/StringProblems.java
+++ src/com/appdevelapp/problems/educative/StringProblems.java
@@ -1,6 +1,9 @@
 package com.appdevelapp.problems.educative;
 
-import java.util.HashSet;
+import com.appdevelapp.datastructures.queue.Queue;
+import com.appdevelapp.datastructures.stack.Stack;
+
+import java.util.*;
 
 public class StringProblems {
     public static String reverse_words (String sentence) {
@@ -58,6 +61,139 @@ public class StringProblems {
         }
     }
 
+    public static boolean can_segment_string(String s, Set<String> dict) {
+
+        for (int i = 1; i <= s.length(); i++) {
+            String left = s.substring(0, i);
+            if(dict.contains(left)){
+                String right = s.substring(i);
+                if(right == null || right == "" || dict.contains(right) || can_segment_string(right, dict)){
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public static XMLTreeNode create_xml_tree(String xml) throws Exception {
+        XMLTreeNode head = null;
+        XMLTreeNode curNode = null;
+        Stack<XMLTreeNode> tags = new Stack<>(10);
+        char[] characters = xml.toCharArray();
+        boolean readingTag = false;
+        String xmlTag = "";
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < characters.length; i++) {
+            char ch = characters[i];
+            if(ch == '\n' || ch=='\t' || ch == ' ') continue;
+
+            if(ch == '<'){
+                if(builder.length() > 0){
+                    XMLTreeNode node = new XMLTreeNode(builder.toString());
+                    tags.top().children.add(node);
+                }
+                readingTag = true;
+                builder = new StringBuilder();
+                continue;
+            }
+
+            if(ch == '>'){
+                String tag = builder.toString();
+                XMLTreeNode node = GetNode(tag);
+                if(head == null){
+                    head = node;
+                    curNode = node;
+                }
+                else{
+                    tags.top().children.add(node);
+                }
+
+                tags.push(node);
+                readingTag = false;
+                builder = new StringBuilder();
+                continue;
+            }
+
+            if(ch == '/'){
+                if(readingTag) {
+                    String matchingTag = tags.top().data;
+                    i = i + matchingTag.length() + 1;
+                    tags.pop();
+                    continue;
+                }
+
+                // Closing Tag
+            }
+
+            if(readingTag){
+                builder.append(ch);
+                continue;
+            }
+            else{
+                builder.append(ch);
+            }
+        }
+
+        return head;
+    }
+
+    // This approach have a cubic run time O(n^3) which is bad.
+    public static int find_all_palindrome_substrings(String input) {
+        int count = 0;
+        int size = 2;
+        while (size <= input.length()) {
+            for (int i = 0; i <= input.length()-size; i++) {
+                String substring = input.substring(i, i+size);
+                if(checkIfPalindrome(substring)){
+                    count++;
+                }
+            }
+
+            size++;
+        }
+        return count;
+    }
+
+    private static boolean checkIfPalindrome(String str){
+        int left = 0;
+        int right = str.length() -1;
+        while (left < right){
+            if(str.charAt(left) != str.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+
+    public static int find_all_palindrome_substrings_2(String input) {
+        int count = 0;
+        for (int i = 0; i < input.length(); i++) {
+            count += find_all_palindrome_substrings_2(input, i-1, i+1);
+            count += find_all_palindrome_substrings_2(input, i, i+1);
+        }
+
+        return count;
+    }
+
+    private static int find_all_palindrome_substrings_2(String input, int left, int right)
+    {
+        int count = 0;
+        for (; left >= 0 && right < input.length() ; left--, right++) {
+            if(input.charAt(left) != input.charAt(right)){
+                break;
+            }
+
+            count++;
+        }
+        return count;
+    }
+    private static XMLTreeNode GetNode(String data){
+        return new XMLTreeNode(data);
+    }
     private static String reverseWordHelper(char[] sentence){
         int length = sentence.length;
         reverseFromStartToEnd(sentence, 0, length-2);
@@ -90,6 +226,45 @@ public class StringProblems {
             sentence[end] = ch;
             start++;
             end--;
+        }
+    }
+}
+
+class XMLTreeNode{
+    String data;
+    List<XMLTreeNode> children;
+
+    public XMLTreeNode(String data){
+        this.data = data;
+        children = new ArrayList<>();
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public List<XMLTreeNode> getChildren() {
+        return children;
+    }
+
+    public void setChildren(List<XMLTreeNode> children) {
+        this.children = children;
+    }
+
+    public void PrintXMLNode() throws Exception {
+        Queue<XMLTreeNode> queue = new Queue<>(10);
+        queue.enqueue(this);
+        while (!queue.isEmpty()){
+            XMLTreeNode curNode = queue.dequeue();
+            for (XMLTreeNode child: curNode.children) {
+                queue.enqueue(child);
+            }
+
+            System.out.println(curNode.data + " --> ");
         }
     }
 }
--- src/com/appdevelapp/problems/educative/StringProblemsTest.java
+++ src/com/appdevelapp/problems/educative/StringProblemsTest.java
@@ -3,6 +3,10 @@ package com.appdevelapp.problems.educative;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import sun.jvm.hotspot.utilities.Assert;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -28,5 +32,58 @@ class StringProblemsTest {
     void remove_white_spaces() {
         String str = "    this is       some sentence  123..!!";
         StringProblems.remove_white_spaces(str.toCharArray());
+    }
+
+    @Test
+    void can_segment_string() {
+        Set<String> dict = new HashSet<>();
+        dict.add("pie");
+        dict.add("pear");
+        dict.add("apple");
+        dict.add("peach");
+        dict.add("hello");
+        dict.add("hell");
+        dict.add("on");
+        dict.add("now");
+        dict.add("o");
+        Assert.that(StringProblems.can_segment_string("applepie", dict), "Failed");
+        Assert.that(!StringProblems.can_segment_string("applepei", dict), "Failed");
+        Assert.that(StringProblems.can_segment_string("hellonowo", dict), "Failed");
+    }
+
+    @Test
+    void create_xml_tree() throws Exception {
+        String xml = "<html>\n" +
+                "  <body>\n" +
+                "    <div>\n" +
+                "      <h1>CodeRust</h1>\n" +
+                "      <a>http://coderust.com</a> \n" +
+                "    </div>\n" +
+                "    <div>\n" +
+                "        <h2>Chapter 1</h2>\n" +
+                "    </div>\n" +
+                "    <div>\n" +
+                "        <h3>Chapter 2</h3>\n" +
+                "        <h4>Chapter 2.1</h4>\n" +
+                "    </div>\n" +
+                "  </body>\n" +
+                "</html>";
+        XMLTreeNode head = StringProblems.create_xml_tree(xml);
+        head.PrintXMLNode();
+        //Assert.that(head.data.equals("html"), "Failed");
+    }
+
+    @Test
+    void find_all_palindrome_substrings() {
+        String str = "aabbaacaabbaa";
+        int result = StringProblems.find_all_palindrome_substrings(str);
+        Assert.that(result == 16, "Failed");
+    }
+
+    @Test
+    void find_all_palindrome_substrings_2() {
+        String str = "aabbaacaabbaa";
+        int result = StringProblems.find_all_palindrome_substrings_2(str);
+        Assert.that(result == 16, "Failed");
     }
 }
