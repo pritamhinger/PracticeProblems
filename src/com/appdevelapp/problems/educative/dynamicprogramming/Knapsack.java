92e58049ae7571cd34b69dccb05c907e036744ff
Pritam Hinger <pritamgenius24@gmail.com>
2020-01-07 22:30:19 +0530

    Adding space optimized solution to Knapsack problem.

--- src/com/appdevelapp/problems/educative/dynamicprogramming/Knapsack.java
+++ src/com/appdevelapp/problems/educative/dynamicprogramming/Knapsack.java
@@ -3,8 +3,8 @@ package com.appdevelapp.problems.educative.dynamicprogramming;
 public class Knapsack {
     public static void main(String[] args) {
         Knapsack ks = new Knapsack();
-        int[] profits = {1, 6, 10, 16, 12};
-        int[] weights = {1, 2, 3, 5, 2};
+        int[] profits = {1, 6, 10, 16};
+        int[] weights = {1, 2, 3, 5};
         int maxProfit = ks.solveKnapsackBruteForce(profits, weights, 7);
         System.out.println("Total knapsack profit ---> " + maxProfit);
         maxProfit = ks.solveKnapsackBruteForce(profits, weights, 6);
@@ -18,6 +18,16 @@ public class Knapsack {
         maxProfit = ks.solveKanpsackBottomUpApproach(profits, weights, 7);
         System.out.println("Total knapsack profit ---> " + maxProfit);
         maxProfit = ks.solveKanpsackBottomUpApproach(profits, weights, 6);
+        System.out.println("Total knapsack profit ---> " + maxProfit);
+
+        maxProfit = ks.solveKanpsackBottomUpApproachModified(profits, weights, 7);
+        System.out.println("Total knapsack profit ---> " + maxProfit);
+        maxProfit = ks.solveKanpsackBottomUpApproach(profits, weights, 6);
+        System.out.println("Total knapsack profit ---> " + maxProfit);
+
+        maxProfit = ks.solveKanpsackBottomUpApproachModified(profits, weights, 7);
+        System.out.println("Total knapsack profit ---> " + maxProfit);
+        maxProfit = ks.solveKanpsackBottomUpApproachModified(profits, weights, 6);
         System.out.println("Total knapsack profit ---> " + maxProfit);
     }
 
@@ -51,7 +61,6 @@ public class Knapsack {
         }
 
         if (distributionMatrix[currentIndex][capacity] != null) {
-            System.out.println("Cache Hit");
             return distributionMatrix[currentIndex][capacity];
         }
 
@@ -77,7 +86,7 @@ public class Knapsack {
         }
 
         for (int index = 0; index <= capacity; index++) {
-            if (weights[0] < index) {
+            if (weights[0] <= index) {
                 distributionMatrix[0][index] = profits[0];
             }
         }
@@ -95,6 +104,54 @@ public class Knapsack {
             }
         }
 
+        PrintSelectedElements(distributionMatrix, profits, weights, capacity);
         return distributionMatrix[profits.length - 1][capacity];
+    }
+
+    private void PrintSelectedElements(int[][] distributionMatrix, int[] profits, int[] weights, int capacity) {
+        int totalProfit = distributionMatrix[profits.length - 1][capacity];
+        for (int index = profits.length - 1; index > 0; index--) {
+            if (totalProfit != distributionMatrix[index - 1][capacity]) {
+                System.out.println("Select item with weight " + weights[index] + ". It have profit of " + profits[index]);
+                totalProfit -= profits[index];
+                capacity -= weights[index];
+            }
+        }
+
+        if (totalProfit != 0) {
+            System.out.println("Select item with weight " + weights[0] + ". It have profit of " + profits[0]);
+        }
+
+        return;
+    }
+
+    public int solveKanpsackBottomUpApproachModified(int[] profits, int[] weights, int capacity) {
+        int[] previous = new int[capacity + 1];
+        int[] current = new int[capacity + 1];
+        int previousProfit = 0;
+
+        for (int i = 0; i <= capacity; i++) {
+            if (weights[0] <= i) {
+                previous[i] = weights[0];
+            }
+        }
+
+        current[0] = previousProfit;
+        for (int i = 1; i < profits.length; i++) {
+            for (int c = 0; c <= capacity; c++) {
+                int profit1 = 0;
+                int profit2 = 0;
+                if (weights[i] <= c) {
+                    profit1 = profits[i] + previous[c - weights[i]];
+                }
+
+                profit2 = previous[c];
+                current[c] = Math.max(profit1, profit2);
+            }
+
+            previous = current;
+        }
+
+        return current[capacity];
     }
 }
